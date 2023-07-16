c95c9518ca965803a4296e195c08ba902a21f49f
José de los santos <delossantosramosjose@gmail.com>
2023-07-16 20:54:14 +0200

    counter simple added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontendmentor-launch-countdown-timer';
+  days = '00'
+  hours = '00'
+  minutes = '00'
+  seconds = '00'
+
+  date = new Date(0,0,0,0,0)
+
+  ngOnInit(){
+    
+    setInterval(()=>{
+      this.date.setSeconds(this.date.getSeconds() +1)
+      this.setTimer()
+    },1000)
+    
+  }
+
+  setTimer(){
+    this.days = this.numberWithZero(this.date.getDay())
+    this.hours = this.numberWithZero(this.date.getHours())
+    this.minutes = this.numberWithZero(this.date.getMinutes())
+    this.seconds = this.numberWithZero(this.date.getSeconds())
+  }
+
+  numberWithZero(number:number):string{
+    return number > 9 ? String(number) : '0' + number
+  }
 }
